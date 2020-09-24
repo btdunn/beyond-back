51aaab808dad34cbf01eeb19f8fba74d6398d877
Ben Dunn <benthedunn@gmail.com>
2020-09-24 10:43:36 -0600

    api link up + env

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const axios = require("axios")
+require('dotenv').config()
+
+const api_key = process.env.API_KEY
 
 app.get('/', (request, response) => {
-  response.json({
-    message: 'hey',
-  })
+  axios.get(`https://aviation-edge.com/v2/public/routes?key=${api_key}`)
+    .then(({ data }) => {
+      response.json({ data })
+    })
 })
 
 app.listen(4000)
